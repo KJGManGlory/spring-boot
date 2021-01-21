21785f2c981f0bbb1498eb0307c20269a461e3b4
lizza <liutao_glory@126.com>
2021-01-21 08:02:07 +0800

    lizza commit.

---
+++ demo/src/main/java/com/lizza/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.lizza.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Desc:
+ * @author: lizza1643@gmail.com
+ * @date: 2020-12-22
+ */
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @PostMapping("msg")
+    public String msg(String msg) {
+        return "The MSG you entered is: " + msg;
+    }
+}
