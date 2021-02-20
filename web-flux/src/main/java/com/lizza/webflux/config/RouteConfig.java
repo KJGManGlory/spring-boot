4ede3f02109f823f7352d56aa3634f163da174c6
lizza <liutao_glory@126.com>
2021-02-20 23:14:37 +0800

    lizza commit.

--- web-flux/src/main/java/com/lizza/webflux/config/RouteConfig.java
+++ web-flux/src/main/java/com/lizza/webflux/config/RouteConfig.java
@@ -2,7 +2,9 @@ package com.lizza.webflux.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.*;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
 
 /**
  * @Desc:
@@ -15,7 +17,8 @@ public class RouteConfig {
     @Bean
     public RouterFunction<ServerResponse> routerFunction() {
         return RouterFunctions.route()
-                .GET("/flux", t -> ServerResponse.ok().body("Hello Web Flux!"))
+                .GET("/flux", t -> ServerResponse.ok().render("msg", "hello webflux"))
+                .filter((request, next) -> null)
                 .build();
     }
 }
